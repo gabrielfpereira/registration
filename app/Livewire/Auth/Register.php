8d2d87350c83e432cd9f436c0e09338df4dd4fa0
gabrielfpereira <gabrielf.pereira@outlook.com>
2025-07-28 23:44:12 -0300

    registrando usuario como supervisor

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -41,6 +41,7 @@ class Register extends Component
             'name'     => $this->name,
             'email'    => $this->email,
             'password' => $this->password,
+            'type'     => 'supervisor',
         ]);
 
         Auth::login($user);
