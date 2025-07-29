23aa26c72769e6a44cc69f4bb5e036d4ab46ca51
gabrielfpereira <gabrielf.pereira@outlook.com>
2025-07-29 17:26:55 -0300

    mudança na visualização do login

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,16 +1,24 @@
 <div>
-    <x-form wire:submit="login">
-        <x-header title="Fazer Login" separator />
-        <x-errors title="Oops!" description="Please, fix them." icon="o-face-frown" />
-    
-        <x-input label="E-mail" wire:model="email" />
-        <x-password label="Password" wire:model="password" clearable />
-    
-        <x-slot:actions>
-            <div class="flex items-center justify-between w-full">
-                <a href="{{ route('auth.recovery-password') }}" class="text-sm text-blue-600 hover:underline">Esqueci minha senha</a>
-                <x-button label="Login" class="btn-primary" type="submit" spinner="login" />
-            </div>
-        </x-slot:actions>
-    </x-form>
+    <div class="flex w-full mb-4 justify-center items-center gap-2">
+        <x-logo />
+        <span class="font-bold me-3 text-3xl bg-gradient-to-r from-blue-500 to-purple-300 bg-clip-text text-transparent ">
+            SOAMAR
+        </span>
+     </div>
+     <x-card title="Fazer Login" separator>
+        <x-form wire:submit="login">
+            <x-errors title="Oops!" description="Please, fix them." icon="o-face-frown" />
+        
+            <x-input label="E-mail" wire:model="email" />
+            <x-password label="Password" wire:model="password" clearable />
+        
+            <x-slot:actions>
+                <div class="flex items-center justify-between w-full">
+                    <a href="{{ route('auth.recovery-password') }}" class="text-sm text-blue-600 hover:underline">Esqueci minha senha</a>
+                    <x-button label="Login" class="btn-primary" type="submit" spinner="login" />
+                </div>
+            </x-slot:actions>
+        </x-form>
+    </x-card>
+
 </div>
