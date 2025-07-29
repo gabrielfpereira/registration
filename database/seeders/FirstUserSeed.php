0c0a91a34d5268b8d0c1123e44a593e4ef22ba65
gabrielfpereira <gabrielf.pereira@outlook.com>
2025-07-29 15:28:27 -0300

    criação de seed para primeiro usuário da aplicação

---
+++ database/seeders/FirstUserSeed.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FirstUserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Here you can create the first user or any initial data you need.
+        // For example:
+        \App\Models\User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@soamar.com',
+            'password' => bcrypt('Password@123'),
+            'type' => 'supervisor'
+        ]);
+    }
+}
