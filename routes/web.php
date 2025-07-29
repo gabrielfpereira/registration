23ff892fb592c5bfb205db24228d661039570780
gabrielfpereira <gabrielf.pereira@outlook.com>
2025-07-28 23:43:40 -0300

    padronizando importação

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
-use App\Livewire\{Dashboard};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/dashboard', Dashboard::class)->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', \App\Livewire\Dashboard::class)->middleware(['auth'])->name('dashboard');
 Route::get('/people', \App\Livewire\People\Index::class)->middleware(['auth'])->name('people');
 Route::get('/registration/items', \App\Livewire\Registration\Items::class)->middleware(['auth'])->name('registration.items');
 Route::get('/registration/create', \App\Livewire\Registration\Create::class)->middleware(['auth'])->name('registration.create');
